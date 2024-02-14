db7eb461f5df871a80cf27e0ad9bc5a7c0c12956
HyeJin Park <hjhelenap@gmail.com>
2024-02-15 00:33:05 +0900

    1. check 버튼 클릭시 해당컨텐츠에 line 토글추가 2. delete 버튼 클릭시 해당 div 삭제

--- main.js
+++ main.js
@@ -5,31 +5,77 @@ addButton.addEventListener("click", addTask);
 
 
 function addTask() {
-    taskList.push(taskInput.value)
-    render()
+    let taskObj = {
+        id : randomIDGenerate(), 
+        taskContent : taskInput.value,
+        isCompleted : false
+    }
+    taskList.push(taskObj)
+    console.log(taskList);
+    render();
 }
 
 function render() {
     let resultHtml ='';
-    // for(let task of taskList){
-    //    resultHtml += `<div class="task">
-    //     <div>${task}</div>
-    //     <div>
-    //         <button>CHECK</button>
-    //         <button>DELETE</button>
-    //     </div>
-    // </div>`;
-    // }
     taskList.forEach(task =>{
-        resultHtml += `<div class="task">
-        <div>${task}</div>
-        <div>
-            <button>CHECK</button>
-            <button>DELETE</button>
-        </div>
-    </div>`;
+        resultHtml += 
+        `<div class="task">`
+        if(task.isCompleted == true) {
+            resultHtml += 
+            `<div class="taskDone">${task.taskContent}</div>`
+        }else {
+            resultHtml += 
+            `<div>${task.taskContent}</div>`
+        }
+        resultHtml += 
+            `<div>
+                <button onclick="toggleComplete('${task.id}')">CHECK</button>
+                <button onclick="deleteTask('${task.id}')">DELETE</button>
+            </div>
+        </div>`; 
     })
 
 
     document.getElementById("taskBoard").innerHTML = resultHtml;
+}
+
+function toggleComplete(id){
+    // for(let task of taskList) {
+    //     if(task.id == id) {
+    //         task.isComplete = true
+    //         break; // for문 종료
+    //     }
+    //     console.log(taskList);
+    // }
+
+    taskList.forEach(task=> {
+        if(task.id == id) {
+            task.isCompleted = !task.isCompleted;
+            render();
+            console.log(taskList);
+        }
+    })
+
+    // for(let i=0; i<taskList.length; i++) {
+    //     if(taskList[i].id == id) {
+    //         taskList[i].isComplete = true;
+    //         break;
+    //     }
+    //     console.log(taskList);
+    // }
+}
+
+function deleteTask(id) {
+   taskList.forEach(task => {
+        if(task.id == id) {
+            taskList.splice(taskList.indexOf(task),1);
+            render();
+            console.log(taskList)
+        }
+   }) 
+}
+
+// 랜덤 id 생성
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2,9); // 함수의 결과물이 다른 데서 쓰일 때에 return 값을 지정
 }
